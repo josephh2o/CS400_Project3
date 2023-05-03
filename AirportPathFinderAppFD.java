804bceb38b2267030ae183af320f610ae6ce4e80
Cameron J <ccjohnson29@wisc.edu>
2023-05-03 18:46:03 -0500

    fixed runner app

--- AirportPathFinderAppFD.java
+++ AirportPathFinderAppFD.java
@@ -6,7 +6,6 @@ import javafx.application.Application;
 public class AirportPathFinderAppFD {
 
 	public static void main( String[] args) {
-	    AirportPathFinderFrontendFD.setBackend(new AirportPath());
 		Application.launch(AirportPathFinderFrontendFD.class);
 	}
 
