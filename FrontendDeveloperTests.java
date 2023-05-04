25677174cdbe8a8e8208d926a3125d8658fc6120
Cameron J <ccjohnson29@wisc.edu>
2023-05-03 20:08:44 -0500

    finished tester methods

--- FrontendDeveloperTests.java
+++ FrontendDeveloperTests.java
@@ -7,6 +7,8 @@ import javafx.scene.text.Text;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+import java.util.List;
+import java.util.ArrayList;
 import edu.wisc.cs.cs400.JavaFXTester;
 
 /**
@@ -62,8 +64,10 @@ public class FrontendDeveloperTests extends JavaFXTester {
 
       clickOn("#choose2");
       
+      // pick 1
       clickOn("#ORD");
       clickOn("#confirm");
+      // pick 2
       clickOn("#SFO");
       clickOn("#confirm");
          
@@ -84,14 +88,17 @@ public class FrontendDeveloperTests extends JavaFXTester {
       clickOn("#choose3");
       assertEquals("Please select an airport", prompt.getText());
       
+      // pick 1
       clickOn("#DEN");
       clickOn("#confirm");
       assertEquals("Please select another airport", prompt.getText());
       
+      // pick 2
       clickOn("#DFW");
       clickOn("#confirm");
       assertEquals("Please select a final airport", prompt.getText());
       
+      // pick 3
       clickOn("#JFK");
       clickOn("#confirm");
       
@@ -112,14 +119,17 @@ public class FrontendDeveloperTests extends JavaFXTester {
       clickOn("#choose3");
       assertEquals("Please select an airport", prompt.getText());
       
+      // pick 1
       clickOn("#ORD");
       clickOn("#confirm");
       assertEquals("Please select another airport", prompt.getText());
       
+      // pick 2
       clickOn("#MCO");
       clickOn("#confirm");
       assertEquals("Please select a final airport", prompt.getText());
       
+      // back to main
       clickOn("#back");
       
       // then try to click on another airport -- should do nothing
@@ -162,6 +172,7 @@ public class FrontendDeveloperTests extends JavaFXTester {
       Text userRouteText = lookup("#userRouteText").query();
       clickOn("#choose2");
       
+      // one route
       clickOn("#DEN");
       clickOn("#confirm");
       clickOn("#DFW");
@@ -183,30 +194,68 @@ public class FrontendDeveloperTests extends JavaFXTester {
     
     /**
      * Tests case if Djikstra's algorithm fails and throws NoSuchElementException;
-     * Backend should propagate the exception
+     * Backend should propagate the exception.
      */
     @Test
     public void test1CodeReviewOfBackendDeveloper() {
       AirportPath backend = new AirportPath();
       
-      assertThrows(NoSuchElementException.class ,() -> backend.getShortestPath("ORD", "DFW") );
+      // no path between ORD and DFW (two airports)
+      assertThrows(NoSuchElementException.class, 
+          () -> backend.getShortestPath("ORD", "DFW") );      
       
+      // no path between SLC, ORD, SFO (three airports)
+      assertThrows(NoSuchElementException.class, 
+          () -> backend.getShortestTrioPath("SLC", "ORD", "SFO") );
     }
     
     
+    /**
+     * Tests the path with the greatest number of nodes and makes sure
+     * backend gives correct path and distance
+     */
     @Test
     public void test2CodeReviewOfBackendDeveloper() {
       AirportPath backend = new AirportPath();
+      List<String> expectedPath = new ArrayList<String>();
+      expectedPath.add("IAD");  
+      expectedPath.add("DEN");
+      expectedPath.add("DFW");
+      expectedPath.add("ORD");
+      expectedPath.add("SFO");
+      
+      assertEquals(expectedPath, backend.getShortestPath("IAD", "SFO"));
+      
+      assertEquals(7630, backend.getShortestDistance("IAD","SFO"));
       
     }
     
     
     /**
      * Tests case where there is no directed route from selected starting/ending points
+     * Tests whether the output can be cleared by the reset map button after such case.
+     * Tests whether the user can create another route after such case.
      */
     @Test
     public void test1Integration() {
       Text userRouteText = lookup("#userRouteText").query();
+      Text prompt = lookup("#prompt").query();
+      
+      clickOn("#choose2");
+      
+      clickOn("#MCO");
+      clickOn("#confirm");
+      clickOn("#IAH");
+      clickOn("#confirm");
+      
+      // ensure no route was found
+      assertEquals("No route found.", userRouteText.getText()); 
+      
+      clickOn("#back");
+      // make sure user can reset the text if desired
+      assertEquals("", userRouteText.getText()); 
+      
+      // get back to "No rotue found" state
       clickOn("#choose2");
       
       clickOn("#MCO");
@@ -214,18 +263,30 @@ public class FrontendDeveloperTests extends JavaFXTester {
       clickOn("#IAH");
       clickOn("#confirm");
          
-      assertEquals("No route found.", userRouteText.getText()); 
-      
-      clickOn("#back");
+      // try switching to airport selection state
+      clickOn("#choose3");
+      assertEquals("Please select an airport", prompt.getText());
     }
     
     
     /**
-     * 
+     * Tests the path with the greatest number of nodes present
+     * and make sure the app handles it correctly (propagates it from AE to FD)
      */
     @Test
     public void test2Integration() {
-
+      Text userRouteText = lookup("#userRouteText").query();
+      clickOn("#choose2");
+      
+      clickOn("#IAD");
+      clickOn("#confirm");
+      clickOn("#SFO");
+      clickOn("#confirm");
+         
+      assertEquals("Route: IAD->DEN->DFW->ORD->SFO"
+          + "\nTotal Distance: 7630 km", userRouteText.getText()); 
+      
+      clickOn("#back");
     }
     
     
